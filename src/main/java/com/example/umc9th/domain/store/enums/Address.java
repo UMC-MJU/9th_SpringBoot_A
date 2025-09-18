f4d97404517de845781145ae85629c386e253b52
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-09-18 21:54:04 +0900

    [CHORE] Address Enum 수정

--- src/main/java/com/example/umc9th/domain/store/enums/Address.java
+++ src/main/java/com/example/umc9th/domain/store/enums/Address.java
@@ -1,5 +1,18 @@
 package com.example.umc9th.domain.store.enums;
 
 public enum Address {
-    강남구, 종로구, 서초구, 은평구
+    GANGNAM("강남구"),
+    JONGNO("종로구"),
+    SEOCHO("서초구"),
+    EUNPYEONG("은평구");
+
+    private final String koreanName;
+
+    Address(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
 }
