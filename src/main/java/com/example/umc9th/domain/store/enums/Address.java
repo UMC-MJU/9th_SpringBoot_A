9b60c27a22df6b1e4083643a547261c9a4149fe4
chang min O <82094699+Ochangmin524@users.noreply.github.com>
2025-09-18 22:19:35 +0900

    [CHORE] Address Enum 값 영어로만 사용하도록 수정

--- src/main/java/com/example/umc9th/domain/store/enums/Address.java
+++ src/main/java/com/example/umc9th/domain/store/enums/Address.java
@@ -1,18 +1,6 @@
 package com.example.umc9th.domain.store.enums;
 
 public enum Address {
-    GANGNAM("강남구"),
-    JONGNO("종로구"),
-    SEOCHO("서초구"),
-    EUNPYEONG("은평구");
+    GANGNAM, JONGNO, SEOCHO, EUNPYEONG
 
-    private final String koreanName;
-
-    Address(String koreanName) {
-        this.koreanName = koreanName;
-    }
-
-    public String getKoreanName() {
-        return koreanName;
-    }
 }
