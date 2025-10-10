1557433afca3772e0f8df2c681697f77f6040888
0711kc <0711kc@naver.com>
2025-10-10 23:23:55 +0900

    refactor: Notification 삭제 시, 하위 알림 엔티티들도 삭제되도록 양방향 매핑/cascade 설정

--- src/main/java/com/example/umc9th/domain/notification/entity/Notification.java
+++ src/main/java/com/example/umc9th/domain/notification/entity/Notification.java
@@ -1,8 +1,15 @@
 package com.example.umc9th.domain.notification.entity;
 
+import java.util.List;
+
 import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.domain.notification.entity.mapping.InquiryAnswer;
+import com.example.umc9th.domain.notification.entity.mapping.NewMission;
+import com.example.umc9th.domain.notification.entity.mapping.RequestReview;
+import com.example.umc9th.domain.notification.entity.mapping.ReviewReply;
 import com.example.umc9th.global.entity.BaseEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,6 +18,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -37,4 +45,16 @@ public class Notification extends BaseEntity {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
+
+	@OneToMany(mappedBy = "notification", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<InquiryAnswer> inquiryAnswers;
+
+	@OneToMany(mappedBy = "notification", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<ReviewReply> reviewReplies;
+
+	@OneToMany(mappedBy = "notification", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<RequestReview> requestReviews;
+
+	@OneToMany(mappedBy = "notification", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<NewMission> newMissions;
 }
