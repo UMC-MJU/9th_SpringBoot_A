16838c2162201dfb491d58e0dbb775520aa9d3cb
0711kc <0711kc@naver.com>
2025-11-06 17:12:59 +0900

    refactor: 반환 타입 Member -> Optional<Member> 수정

--- src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package com.example.umc9th.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.umc9th.domain.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-	Member findMemberById(long id);
+	Optional<Member> findMemberById(long id);
 }
